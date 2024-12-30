7e72ac432f5512d78b15a3718620eb5906356e41
Collin Murch <collin@murch.net>
2024-12-30 10:26:13 -0800

    debugging theming

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -45,7 +45,50 @@ return {
 				ui.close()
 			end
 
+			-- Theming
 			vim.fn.sign_define("DapBreakpoint", { text = "◌" })
+
+			vim.api.nvim_set_hl(0, "DapUIScope", { link = "DiagnosticInfo" })
+			vim.api.nvim_set_hl(0, "DapUIValue", { link = "DiagnosticFloatingInfo" })
+			vim.api.nvim_set_hl(0, "DapUIModifiedValue", { link = "DiagnosticInfo" })
+			vim.api.nvim_set_hl(0, "DapUIDecoration", { link = "DiagnosticInfo" })
+
+			vim.api.nvim_set_hl(0, "DapUIThread", { link = "DiagnosticOk" })
+			vim.api.nvim_set_hl(0, "DapUIStoppedThread", { link = "DiagnosticWarn" })
+
+			vim.api.nvim_set_hl(0, "DapUIType", { link = "DiagnosticHint" })
+			vim.api.nvim_set_hl(0, "DapUISource", { link = "DiagnosticHint" })
+			vim.api.nvim_set_hl(0, "DapUILineNumber", { link = "LineNr" })
+
+			vim.api.nvim_set_hl(0, "DapUIWatchesEmpty", { link = "DiagnosticError" })
+			vim.api.nvim_set_hl(0, "DapUIWatchesValue", { link = "DiagnosticOk" })
+			vim.api.nvim_set_hl(0, "DapUIWatchesError", { link = "DiagnosticError" })
+
+			vim.api.nvim_set_hl(0, "DapUIBreakpointsPath", { link = "DiagnosticInfo" })
+			vim.api.nvim_set_hl(0, "DapUIBreakpointsInfo", { link = "DiagnosticOk" })
+			vim.api.nvim_set_hl(0, "DapUIBreakpointsDisabledLine", { link = "Comment" })
+			vim.api.nvim_set_hl(0, "DapUIBreakpointsCurrentLine", { link = "DiagnosticOk" })
+
+			vim.api.nvim_set_hl(0, "DapUIStepOver", { link = "DiagnosticInfo" })
+			vim.api.nvim_set_hl(0, "DapUIStepInto", { link = "DiagnosticInfo" })
+			vim.api.nvim_set_hl(0, "DapUIStepBack", { link = "DiagnosticInfo" })
+			vim.api.nvim_set_hl(0, "DapUIStepOut", { link = "DiagnosticInfo" })
+			vim.api.nvim_set_hl(0, "DapUIStop", { link = "DiagnosticError" })
+			vim.api.nvim_set_hl(0, "DapUIPlayPause", { link = "DiagnosticOk" })
+			vim.api.nvim_set_hl(0, "DapUIRestart", { link = "DiagnosticOk" })
+			vim.api.nvim_set_hl(0, "DapUIUnavailable", { link = "Comment" })
+
+			vim.api.nvim_set_hl(0, "DapUIPlayPauseNC", { link = "DiagnosticOk" })
+			vim.api.nvim_set_hl(0, "DapUIRestartNC", { link = "DiagnosticOk" })
+			vim.api.nvim_set_hl(0, "DapUIStopNC", { link = "DiagnosticError" })
+			vim.api.nvim_set_hl(0, "DapUIUnavailableNC", { link = "Comment" })
+			vim.api.nvim_set_hl(0, "DapUIStepOverNC", { link = "DiagnosticInfo" })
+			vim.api.nvim_set_hl(0, "DapUIStepIntoNC", { link = "DiagnosticInfo" })
+			vim.api.nvim_set_hl(0, "DapUIStepBackNC", { link = "DiagnosticInfo" })
+			vim.api.nvim_set_hl(0, "DapUIStepOutNC", { link = "DiagnosticInfo" })
+
+			vim.api.nvim_set_hl(0, "DapUIFloatBorder", { link = "FloatBorder" })
+			vim.api.nvim_set_hl(0, "DapUIWinSelect", { link = "DiagnosticInfo" })
 		end,
 	},
 }
