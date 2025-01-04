43981296e8e6d0a8ac685a9081f49b3dc9c206ae
Collin Murch <collin@murch.net>
2025-01-03 17:56:09 -0800

    fix some stuff

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,17 +5,30 @@ return {
 		{ "echasnovski/mini.icons", opts = {} },
 	},
 	opts = {
+		delete_to_trash = true,
+		skip_confirm_for_simple_edits = true,
 		view_options = {
 			show_hidden = true,
+			natural_order = true,
+			is_always_hidden = function(name, _)
+				return name == ".." or name == ".git"
+			end,
+		},
+		float = {
+			padding = 5,
+			max_width = 90,
+		},
+		win_options = {
+			wrap = true,
 		},
 	},
 	keys = {
-        {
-            "\\",
-            function()
-                require("oil").toggle_float()
-            end,
-            desc = "Toggle Oil file explorer",
-        },
-    },
+		{
+			"\\",
+			function()
+				require("oil").toggle_float()
+			end,
+			desc = "Toggle Oil file explorer",
+		},
+	},
 }
