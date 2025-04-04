d1a08a508e9ad907a8f990304b09f6035c73d0e3
Collin Murch <collin@murch.net>
2025-04-03 20:22:00 -0500

    system clipboard

--- lua/opt.lua
+++ lua/opt.lua
@@ -44,11 +44,11 @@ vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 -- Preview substitutions live, as you type
 vim.opt.inccommand = "split"
 
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
 -- Go to previous/next line with h,l,left arrow and right arrow
 vim.opt.whichwrap:append("<>[]hl")
+
+-- Use system clipboard
+vim.opt.clipboard = "unnamedplus"
