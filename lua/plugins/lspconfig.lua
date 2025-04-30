88718767df3c697b80c34db1a59c7344b114dd84
Collin Murch <collin@murch.net>
2025-04-29 23:02:17 -0500

    fix typescript

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -55,7 +55,7 @@ return {
 					},
 				},
 			},
-			tsserver = {
+			vtsls = {
 				settings = {
 					typescript = {
 						inlayHints = {
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,8 @@ return {
 	build = ":TSUpdate",
 	main = "nvim-treesitter.configs",
 	opts = {
+	    -- Doesn't automatically install for Svelte Typescript files, breaking highlighting
+	    ensure_installed = { 'typescript' },
 		highlight = {
 			enable = true,
 			additional_vim_regex_highlighting = false,
